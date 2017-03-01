d355ec8f319a170f4ec2e0cae1d0dbc1488ab5f4
Joe Williams <joewilliamsis@live.com>
2017-03-01 00:22:24 -0500

    Add files via upload
    Update 2 : 
    added Entanglement (Still working on the entangle result factors)
    added different flips based on my Q-chart.

--- Quantum.js
+++ Quantum.js
@@ -1,26 +1,46 @@
 /*
 This is Artificial Quantum Computing Mechanics (AQCM) created by Joe Williams(shadowkingbubba)
 
-This displays my understanding of quantum physics.. Even though most of it is simplified. :(
+This displays my understanding of quantum physics.. Even though most of it is simplified.
 
-FIXES NEEDED: Entanglement errors, Add the possibility vectors, Make random value (1/sqrt)
+Each Qubit has 2 basic values the probability and a random number between 1 and 0;
+
+Basically what I did was break down the gates.
+
+          "xFlip"
+__________________   /   "xyflip" 0 > (x*y)
+          0
+          |           | "yFlip"
+          |           |
+-.5_______|_______.5  |
+          |           |
+          |           |
+         +-1          |
+                      \"flipXy" x * y
+
+FIXES NEEDED: Entanglement errors, Add the possibility vectors, Make random value (1/sqrt), add parallel programming
+
+The meaning of this code is to narrow down the probability to specify a certain value using "Artificial Quantum Physics"
 
 @author shadowkingbubba
 */
 
+var values = [];
+
 function setup() {
+  createCanvas(window.innerWidth,window.innerHeight);
   var arr1 = [];
   var arr2 = [];
-  run(100, function(q) {
+  run(5000, function(q) {
     // n qubits : q[n-1]                -H-||||||-H-
     // Example Deutsch's Algorithm -    -H-||||||---
-    var qb1 = createQubit();
-    var qb2 = createQubit();
+    var qb1 = new Qubit();
+    var qb2 = new Qubit();
     qb1.cH(.5);       // Spin right
     qb2.cH(-.5);      // Spin left
     //BLOCK
-    //qb2.cNOT(qb1);   //          -H-|-.---|-H-
-    //qb2.NOT();    //          -H-|-+-+-|---
+    qb2.cNOT(qb1);   //          -H-|-.---|-H-
+    qb2.NOT();    //          -H-|-+-+-|---
     //BLOCK
     qb1.H();         // reads sets
 
@@ -28,12 +48,22 @@ function setup() {
     arr2.push(qb2.M());
   });
   var avg1 = 0.00; var avg2 =0.00;
-  for(var i = 0; i < arr1.length; i++) { avg1 += arr1[i]; console.log(arr1[i]); }
-  for(var i = 0; i < arr2.length; i++) { avg2 += arr2[i]; console.log(arr2[i]); }
-  console.log((avg1/arr1.length)+" AND "+(avg2/arr2.length));
+  for(var i = 0; i < arr1.length; i++) { avg1 += arr1[i];}
+  for(var i = 0; i < arr2.length; i++) { avg2 += arr2[i];}
+  values[0] = (avg1/arr1.length);
+  values[1] = (avg2/arr2.length);
+}
+
+function draw() {
+  background(0);
+  fill(155,23,12);
+  textSize(50);
+  text(values[0], width/2, (1.5*height)/4);
+  text(values[1], width/2, (2.5*height)/4);
 }
 
 function Qubit() {
+  this.p = random(1);
   this.psi = 0; // value between -1 and 1
 
 // Set to the default super position and takes it out
@@ -45,6 +75,7 @@ function Qubit() {
     } else
       return 1;
   }
+
   // p = superposition's position/probability "add more properties"
   this.cH = function cH(p) {
     this.psi = p;
@@ -54,22 +85,18 @@ function Qubit() {
     this.NOT();
     this.psi = sqrt(this.psi);
   }
+
   // Tie to another qubit to entangle
   this.NOT = function NOT() {
-    if(this.psi < 0){
-      var x = this.psi - -.5;
-      this.psi -= x;
-    }
-    else {
-      var x = this.psi - .5;
-      this.psi -= x;
-    }
+    this.xFlip();
   }
-  // Controlled not gate
+
+  // Controlled not gate "Add Entanglement"
   this.cNOT = function cNOT(q) {
-    q.psi = -q.psi;
-    if(collapse(this.psi) == 1)
-      this.NOT();
+    q.yFlip();
+    if(this.entangle(q)) {
+
+    }
   }
   // Swaps the gate value
   this.SWAP = function(q) {
@@ -80,8 +107,76 @@ function Qubit() {
   }
   // Measures the value of the qubit and flushes the value
   this.M = this.measure = function M() {
-    this.psi = collapse(this.psi);
+    this.psi = this.collapse();
     return this.psi;
+  }
+  this.entangle = function entangle(q,s) {
+    if(q.psi >= 0)
+      if(s == 1) {
+        this.p = q.p;
+      } else if (s == 0) {
+        this.p = q.p;
+        this.yFlip();
+      }
+    else
+      if (s == 1) {
+        this.p = q.p;
+      } else if (s == 0) {
+        this.yFlip();
+      }
+  }
+
+  this.xFlip = function xFlip() {
+    if(this.psi < 0){
+      var x = this.psi - -.5;
+      this.psi -= x;
+    } else {
+      var x = this.psi - .5;
+      this.psi -= x;
+    }
+  }
+
+  this.yFlip = function yFlip() {
+    this.psi = -this.psi;
+  }
+
+  this.xyFlip = function xyflip() {
+    if(this.psi == .75 || this.psi == -.25 )
+      return this.psi;
+    if(this.psi < .5) { // Set the axis to -.25
+      var x = this.psi - -.25;
+      this.psi = -x + -.25;
+    } else { // Set the axis to .75
+      var x = this.psi - .75;
+      this.psi = -x + .75;
+    }
+  }
+
+  this.flip = function flip() {
+    if(this.psi == -.75 || this.psi == .25 )
+      return this.psi;
+    if(this.psi < -.5) { // Set the axis to -.25
+      var x = this.psi - .25;
+      this.psi = -x + .25;
+    } else { // Set the axis to .75
+      var x = this.psi - -.75;
+      this.psi = -x + -.75;
+    }
+  }
+
+  // Qubit value flush, pushes the value to either 1 or 0
+  this.collapse = function collapse() {
+    // for instance : p = .5 : p = 50%
+    var r = this.p;
+    var p = this.psi;
+    if(p <= 0)
+      if(abs(p) > r)
+        return 1;
+      else return 0;
+    else
+      if(p > r)
+        return 1;
+      else return 0;
   }
 }
 
@@ -89,29 +184,3 @@ function Qubit() {
 function run(times, qasm) {
   for(var i = 0; i < times; i++) qasm(this.q);
 }
-
-function createQubit() {
-  return new Qubit();
-}
-
-// Qubit value flush, pushes the value to either 1 or 0
-function collapse(p) {
-  // for instance : p = .5 : p = 50%
-  r = random(1)
-  if(p < 0)
-    if(abs(p) > r)
-      return 1; else return 0;
-  else
-    if(p > r)
-      return 1; else return 0;
-}
-/*
-absolute value of X
-function abs(x) {
-  return Math.abs(x);
-}
-// Square root of X
-function sqrt(x) {
-  return Math.sqrt(x);
-}
-*/
